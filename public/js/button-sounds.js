4a939011dc9695670d975e4c30b38b2a331b095d
William Norman <lordcrekit@gmail.com>
2017-02-14 11:09:17 -0500

    Done.

--- public/js/button-sounds.js
+++ public/js/button-sounds.js
@@ -1,11 +1,15 @@
-for (i = 0; i < 3; i++) {
+function addButtonSounds(i) {
   var button = document.getElementById('b0'.concat(i.toString()));
-  var clickAudio = new Audio('/public/audio/click0'.concat(i.toString()).concat('.wav'));
-  var mouseenterAudio = new Audio('/public/audio/hover0'.concat(i.toString()).concat('.wav'));
+  var clickAudio = new Audio('public/audio/click0'.concat(i.toString()).concat('.wav'));
   button.addEventListener('click', function () {
     clickAudio.play();
   });
+  var mouseenterAudio = new Audio('public/audio/hover0'.concat(i.toString()).concat('.wav'));
   button.addEventListener('mouseenter', function () {
     mouseenterAudio.play();
   });
+}
+
+for (i = 0; i < 3; i++) {
+  addButtonSounds(i);
 }
